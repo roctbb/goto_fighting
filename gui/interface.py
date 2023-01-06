390f2ff486566dad19c8c3a509659109cfcf6d14
krekerysu <dimasidorovsciy@gmail.com>
2023-01-06 14:25:10 +0300

    Прогрессбары правильного размера

--- gui/interface.py
+++ gui/interface.py
@@ -2,6 +2,9 @@ from domain.object import Object
 from domain.skin import Skin
 from domain.states import MoveState, HitState, Direction
 from gui.screen import Screen
+
+
+
 class Interface:
     def __init__(self, player1, player2, screen: Screen):
         self.__screen = screen
@@ -10,14 +13,16 @@ class Interface:
 
     def draw(self):
         color = "red"
-        rect = self.__screen.canvas.create_rectangle(0, 0, 200, 50,
+
+        one_hp = (self.__screen.width // 2 - 50) // 100
+        rect = self.__screen.canvas.create_rectangle(0, 0, self.__player1.hp * one_hp, 100,
                                                     fill=color)
         self.__screen.add_object(rect)
 
         # self.__screen.width
         # self.__screen.height
 
-        rect = self.__screen.canvas.create_rectangle(440, 50, 644, 0,
+        rect = self.__screen.canvas.create_rectangle(self.__screen.width - self.__player2.hp * one_hp, 0, self.__screen.width, 100,
                                                     fill=color)
         self.__screen.add_object(rect)
 
